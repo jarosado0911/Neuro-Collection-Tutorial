cc1608a9b385b2f14afb7eea2bb5658e14893515
James Rosado <jamesrosado@Jamess-iMac.local>
2020-10-04 17:26:43 -0400

    updated scripts

--- Example-Cable-Neuron/runscript.sh
+++ Example-Cable-Neuron/runscript.sh
@@ -1,1 +1,2 @@
-ugshell -ex single_neuron_somaInjection.lua -grid pyramidal.ugx -dt 1e-5 -endTime 0.05 -vtk -pstep 1e-4 -outName output
+#!/bin/sh
+/path-to-ugshell/bin/ugshell -ex single_neuron_somaInjection.lua -grid pyramidal.ugx -dt 1e-5 -endTime 0.05 -vtk -pstep 1e-4 -outName output
--- Example-Ion-Dynamics/runscript.sh
+++ Example-Ion-Dynamics/runscript.sh
@@ -1,4 +1,5 @@
-ugshell -ex single_neuron_ionDynamics.lua -grid mouse4_standard.ugx -numRefs 0 -dt 0.00001 -endTime 0.05 -vtk -pstep 0.0001 -outName output -nSyn 50 -avgStart 0.000 -avgDur 0.05
+#!/bin/sh
+/path-to-ugshell/bin/ugshell -ex single_neuron_ionDynamics.lua -grid mouse4_standard.ugx -numRefs 0 -dt 0.00001 -endTime 0.05 -vtk -pstep 0.0001 -outName output -nSyn 50 -avgStart 0.000 -avgDur 0.05
 
 
 
