447bad1bfe73cc348f330543923c56422cf69450
James Rosado <57325500+jarosado0911@users.noreply.github.com>
2020-10-05 11:49:47 -0400

    Delete runscript.sh

--- Example-Spine-Dend/runscript.sh
+++
@@ -1,2 +0,0 @@
-ugshell -ex reconstructed_spine_wER.lua -grid Spine5_wER.ugx -numRefs 0 -caInflux 0.0086 -tstep 2.5e-6 -endTime 0.015 -setting ryrserca -ryrDensity 0.009 -minDef 1e-12 -outName output -solver GS -vtk -pstep 1e-4
-
